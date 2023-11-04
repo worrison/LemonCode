bbe9ff8cc4a8c9dbfea14cbf3f3fe382f2da7e4e
victor ordax <victorordaxantolin@gmail.com>
2023-11-04 23:30:31 +0100

    maquina tragaperras

--- LENGUAJES/03-playground-ejercicios/src/02-entregables/ejercicio-05.ts
+++ LENGUAJES/03-playground-ejercicios/src/02-entregables/ejercicio-05.ts
@@ -1,1 +1,31 @@
 console.log("************** DELIVERABLE 05 *********************");
+console.log("El objetivo de este ejercicio es crear una máquina tragaperras utilizando clases donde cada vez que juguemos insertemos una moneda. Cada máquina tragaperras (instancia) tendrá un contador de monedas que automáticamente se irá incrementando conforme vayamos jugando.Cuando se llame al método play el número de monedas se debe incrementar de forma automática y debe generar tres booleanos aleatorios que representarán el estado de las 3 ruletas. El usuario habrá ganado en caso de que los tres booleanos sean true, y por tanto deberá mostrarse por consola el mensaje:");
+
+
+class SlothMachine {
+    coins: number;
+    constructor() {
+        this.coins = 0;
+    }
+
+    play() {
+        this.coins++;
+        let ruleta1 = Math.random() >= 0.5;
+        let ruleta2 = Math.random() >= 0.5;
+        let ruleta3 = Math.random() >= 0.5;
+
+        if (ruleta1 && ruleta2 && ruleta3) {
+            console.log(` ${ruleta1} | ${ruleta2} | ${ruleta3}   -> Congratulations!!!. You won ${this.coins} coins!!`);
+            this.coins = 0;
+        } else {
+            console.log(` ${ruleta1} | ${ruleta2} | ${ruleta3}   -> Good luck next time!!`);
+        }
+    }
+}
+
+const machine1 = new SlothMachine();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
