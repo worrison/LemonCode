78d94442a08e80602322799a43df55dda7622ddc
victor ordax <victorordaxantolin@gmail.com>
2023-12-14 17:33:32 +0100

    correciones pero aun con errores de: Automatic publicPath is not supported in this browser

--- BUNDLING/Entrega/1-Basico/webpack.config.js
+++ BUNDLING/Entrega/1-Basico/webpack.config.js
@@ -1,14 +1,28 @@
-const path = require('path');
-module.exports = {
-    resolve: {
-        extensions: [".js", ".ts"]
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import url from "url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+
+export default {
+    mode: 'development',
+    context: path.resolve(__dirname, "./src"),
+    resolve:{
+        extensions: ['.ts', '.js'],
     },
-    entry: './src/index.ts',
+    entry: {
+        app: "./index.ts",
+    },
+    output: {
+        filename: "[name].[chunkhash].js",
+        path: path.resolve(__dirname, "dist"),
+      }, 
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.(png|jpg)$/,
-                type: 'asset/resource'
-            },
+                type: "asset/resource",
+              },
             {
                 test: /\.html$/,
                 loader: 'html-loader',
@@ -19,15 +33,30 @@ module.exports = {
                 use: ["style-loader", "css-loader", "sass-loader"],
             },
             {
-                        test: /\.tsx?$/,
-                        exclude: /node_modules/,
-                        loader: "babel-loader"
-            },
+                test: /\.(js|ts|tsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+              },
         ]
     },
     devServer: {
+        port: 8080,
+        open: true,
+        hot: true,
         static: {
-            directory: path.join(__dirname, './src'),
+          directory: path.join(__dirname, "src"),
         },
-    }
+        devMiddleware: {
+          stats: "errors-only",
+        },
+      },
+    plugins: [
+        //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+          filename: "index.html", //Nombre del fichero dentro de ./dist/
+          template: "./index.html", //Nombre del fichero dentro de ./src
+          scriptLoading: "blocking", // Just use the blocking approach (no modern defer or module)
+          hash:true
+        }),
+      ],
 }
