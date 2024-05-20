ec9d157f973d0fb3a9a907b2406cf2a88659004a
victor ordax <victorordaxantolin@gmail.com>
2024-05-20 22:39:15 +0200

    cambio de cabecera si esta logueado o no

--- FRAMEWORKS/ANGULAR/mini-aplicacion/src/app/app.component.ts
+++ FRAMEWORKS/ANGULAR/mini-aplicacion/src/app/app.component.ts
@@ -3,14 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/components/header/header.component';
 import { HeaderPrivateComponent } from './core/components/header-private/header-private.component';
 import { FooterComponent } from './core/components/footer/footer.component';
+import { AuthService } from './services/auth.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,HeaderPrivateComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,HeaderPrivateComponent,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mini-aplicacion';
+  constructor(public authService: AuthService) { }
+
 }
