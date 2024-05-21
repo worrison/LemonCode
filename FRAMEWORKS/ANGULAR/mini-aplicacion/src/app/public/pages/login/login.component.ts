47387a6c4570a7f15e5b33e8a29158917f06f90f
victor ordax <victorordaxantolin@gmail.com>
2024-05-21 21:53:16 +0200

    mensaje con usuario y password, alineacion mensaje login incorrecto

--- FRAMEWORKS/ANGULAR/mini-aplicacion/src/app/public/pages/login/login.component.ts
+++ FRAMEWORKS/ANGULAR/mini-aplicacion/src/app/public/pages/login/login.component.ts
@@ -3,13 +3,14 @@ import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angula
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule,MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule,MatButtonModule,MatCardModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
